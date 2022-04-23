aac516d84d17388a3f23b841a4e8a0f7dce830f6
Hrvoje <zsnopek@gmail.com>
2022-04-23 16:48:56 +0200

    init

---
+++ web/src/pages/SimulateSendMessageToOkpPage/SimulateSendMessageToOkpPage.js
@@ -0,0 +1,45 @@
+import React, {useEffect, useState} from "react";
+import axios from "../../utils/axios-instance";
+
+function SimulateSendMessageToOkpPage(props) {
+
+    const [response, setResponse] = useState('');
+    const [oib, setOib] = useState('');
+
+    useEffect(() => {
+        console.log(oib, 'oib');
+    }, [oib]);
+
+    const handleSendMessageToOkp = () => {
+        axios.post("/okp/send-message/" + oib).then( res => {
+            setResponse(res.data);
+            alert('message successfully sent!');
+        })
+    }
+
+    const handleInputChange = ({target: {name: name, value: value}}) => {
+        setOib(value);
+    }
+
+    return (
+        <div style={{display: "flex", flexDirection: "column", height: "100%", marginTop: "200px"}}>
+            <input
+                type={"text"}
+                style={{width:"200px", alignSelf:"center"}}
+                placeholder={"OIB"}
+                id={"oib"}
+                onChange={handleInputChange}
+            />
+            <button
+                type="button"
+                className="btn btn-primary"
+                style={{width:"200px", alignSelf:"center", marginTop: '4px'}}
+                onClick={() => handleSendMessageToOkp()}>Send message
+            </button>
+
+            <p>{response}</p>
+        </div>
+    );
+}
+
+export default SimulateSendMessageToOkpPage;
